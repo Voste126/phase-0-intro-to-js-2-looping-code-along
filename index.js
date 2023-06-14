4ce10f2f43265f7b984935998289268e5e209b1f
voste126 <steveaustine126@gmail.com>
2023-06-14 18:01:22 +0300

    looops

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];//declare an empty array
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;//store the values
+      messages.push(message);//push the storesd messages
+    }
+    return messages;
+  }
+  //countdown
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
